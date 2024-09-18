3e2be55b3b4a9adee0ff385e6a70b6a9ac569e49
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-18 10:04:00 +0200

    modified:    a function to generate password

--- generator.py
+++ generator.py
@@ -8,10 +8,10 @@ chars = ''
 
 print('Hello, I will help you to generate a password for you, but now answer to questions, which will help me to help you :)')
 print('How much passwords do you need?')
-count = input()
+count = int(input())
 print('Write please Length of your password')
 length = int(input())
-digits = input('Digits - 0123456789? - yes/no: ')
+digits_answ = input('Digits - 0123456789? - yes/no: ')
 
 up_letters = input('Upper letters - ABCDEFGHIJKLMNOPQRSTUVWXYZ? - yes/no: ')
 
@@ -20,3 +20,36 @@ low_letters = input('Lower letters -  abcdefghijklmnopqrstuvwxyz? - yes/no: ')
 symbols = input('Symbols - !#$%&*+-=?@^_?  - yes/no: ')
 
 ex_simbols = input('Ambiguous symbols? - il1Lo0O? - yes/no: ')
+
+def create_char(chars):
+  if digits_answ.lower() == 'yes':
+    chars += digits
+  if up_letters.lower() == 'yes':
+    chars += uppercase_letters
+  if low_letters.lower() == 'yes':
+    chars += lowercase_letters 
+  if symbols.lower() == 'yes':
+    chars += punctuation
+  if ex_simbols.lower() == 'yes':
+    chars = chars.replace('i', '')
+    chars = chars.replace('l', '')
+    chars = chars.replace('1', '')
+    chars = chars.replace('L', '')
+    chars = chars.replace('o', '')
+    chars = chars.replace('0', '')
+    chars = chars.replace('O', '')
+  return chars
+
+def generator(length, chars):
+  result = ''
+  for i in range(0, length):
+    result += random.choice(chars)
+  return result
+
+chars = create_char(chars)
+
+for _ in range(count):
+   password = generator(length, chars)
+   print('Generated password:', password)  
+
+
